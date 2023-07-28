653e5b4acc9e6110daf129b0ad823cc745147622
FelipeWeek066 <felipe1smg1g@gmail.com>
2023-07-28 08:46:16 -0400

    h2, test Profile, JPA

--- src/main/java/com/Study/Course/entities/User.java
+++ src/main/java/com/Study/Course/entities/User.java
@@ -2,12 +2,19 @@ package com.Study.Course.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "User")
 public class User implements Serializable{
-	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
